be723b5eabf555b2f1f76e626928fbd4c0d78561
dadiorchen <antlet2004@gmail.com>
2024-11-26 03:11:31 +0000

    chore: remove raw column

--- timescale/db.sql
+++ timescale/db.sql
@@ -1,10 +1,9 @@
 --- create table for heartbeats signal
-CREATE TABLE heartbeats (
+CREATE TABLE events (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     data TEXT NOT NULL,
     eui TEXT NOT NULL,
-    raw JSONB NOT NULL,
     time TIMESTAMP NOT NULL
 );

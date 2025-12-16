603f0298720a1cf19700ce9e30dadb4e472f9861
Zaven Arra <zaven.arra@gmail.com>
2025-12-16 14:16:49 -0800

    wip: adding rriv-api client to keycloak

--- 02-tf/modules/keycloak/clients.tf
+++ 02-tf/modules/keycloak/clients.tf
@@ -7,6 +7,9 @@ resource "keycloak_openid_client" "rrivctl" {
   standard_flow_enabled = true
   direct_access_grants_enabled = true
   full_scope_allowed = false
+  access_token_lifespan = 8640000
+  client_session_idle_timeout = 864000 
+  client_session_max_lifespan = 8640000
 
   valid_redirect_uris = [
     "${local.rrivctl_redirect_relative_uri}/*",
@@ -14,6 +17,29 @@ resource "keycloak_openid_client" "rrivctl" {
   ]
 
   base_url = "https://${local.rrivctl_subdomain}.${var.domain}"
+}
+
+resource "keycloak_openid_client" "rriv-api" {
+  realm_id            = keycloak_realm.rriv_beta.id
+  client_id           = "rriv-api"
+  name                = "$${client_account}"
+  enabled             = true
+  access_type         = "CONFIDENTIAL"
+  full_scope_allowed = false
+  service_accounts_enabled = true
+
+  valid_redirect_uris = [
+    "${local.rrivctl_redirect_relative_uri}/*",
+    "/oidc/callback"
+  ]
+
+  base_url = "https://${local.rrivctl_subdomain}.${var.domain}"
+}
+
+resource "keycloak_openid_client_service_account_realm_role" "client_service_account_role" {
+  realm_id                = keycloak_realm.rriv_beta.id
+  service_account_user_id = keycloak_openid_client.rriv-api.service_account_user_id
+  role                    = "manage-users"
 }
 
 # # Prod-only module
